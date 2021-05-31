770cff23136455ef534814e35b23b53afaad2b59
Suren Rodrigo <bksdrodrigo@outlook.com>
2021-05-31 14:16:54 +0530

    1.0.7

--- src/common-middleware/logger.middleware.ts
+++ src/common-middleware/logger.middleware.ts
@@ -8,7 +8,6 @@ const stream: StreamOptions = {
   write: (message) => logHttp(message),
 };
 
-
 // Skip all the Morgan http log if the
 // application is not running in development mode.
 // This method is not really needed here since
@@ -17,7 +16,7 @@ const stream: StreamOptions = {
 const skip = () => {
   // const env = process.env.NODE_ENV || 'development';
   // return env !== 'development';
-  // TODO: Make it a appliction level configuration managed through env variables 
+  // TODO: Make it a appliction level configuration managed through env variables
   // TODO: i.e. to skip logs in production. For now we allow all logs
   return false;
 };
