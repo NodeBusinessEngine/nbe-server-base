9e5d16e6518c6ba4a7ee6a7fd0c5693ddded4a3a
Suren Rodrigo <bksdrodrigo@outlook.com>
2021-05-31 14:16:37 +0530

    made the logs available in production

--- src/common-middleware/logger.middleware.ts
+++ src/common-middleware/logger.middleware.ts
@@ -8,14 +8,18 @@ const stream: StreamOptions = {
   write: (message) => logHttp(message),
 };
 
+
 // Skip all the Morgan http log if the
 // application is not running in development mode.
 // This method is not really needed here since
 // we already told to the logger that it should print
 // only warning and error messages in production.
 const skip = () => {
-  const env = process.env.NODE_ENV || 'development';
-  return env !== 'development';
+  // const env = process.env.NODE_ENV || 'development';
+  // return env !== 'development';
+  // TODO: Make it a appliction level configuration managed through env variables 
+  // TODO: i.e. to skip logs in production. For now we allow all logs
+  return false;
 };
 
 // Build the morgan middleware
